8126f0b0ee9e45127b14c543a99a34a42e9aa227
Clown-Box <Sm3n26@gmail.com>
2023-09-08 13:36:15 -0400

    First Update

--- Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -7,10 +7,12 @@ print("I have learned the following:")
 print("What features make Swift a modern and safe language")
 print("How to use the Swift REPL in Terminal")
 print("How to use playgrounds to make writing Swift fun and simple")
+
+
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("rip me out the plastic")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
